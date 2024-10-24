10532c96f242f2ae63c35ee7ec350dd03ea37a97
MohammadNasir520 <nasirahsan520@gmail.com>
2024-10-24 13:47:58 +0600

    min max learn

--- C_Programming.c
+++ C_Programming.c
@@ -56,17 +56,39 @@ int main()
 
 // ..............min , max output from array............
 
+// int n;
+// scanf("%d", &n);
+// int a[n];
+// for( int i=1; i<=n; i++){
+//     scanf("%d", &a[i]);
+//     // printf("%d ",a[i]);
+// }
+
+// int min = INT_MAX , max= INT_MIN;
+
+// for (int i = 1; i<= n; i++){
+//     if( a[i]< min ){
+//         min= a[i];
+//     }
+//     if( a[i]> max ){
+//         max= a[i];
+//     }
+// }
+
+// printf("min %d , max %d",min , max);
+
+// ............another aproachec..............
 int n;
 scanf("%d", &n);
 int a[n];
-for( int i=1; i<=n; i++){
+for( int i=0; i<n; i++){
     scanf("%d", &a[i]);
     // printf("%d ",a[i]);
 }
 
-int min = INT_MAX , max= INT_MIN;
+int min = a[0] , max= a[0];
 
-for (int i = 1; i<= n; i++){
+for (int i = 0; i< n; i++){
     if( a[i]< min ){
         min= a[i];
     }
