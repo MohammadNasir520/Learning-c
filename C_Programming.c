b592ea566949c8b169655ba744f0bdbee8e0f2bc
MohammadNasir520 <nasirahsan520@gmail.com>
2024-10-24 13:01:44 +0600

    first commit

---
+++ C_Programming.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    // int n;
+
+    // scanf("%d", &n);
+
+    // int a[n];
+
+    // for (int i = 0; i < n; i++) {
+    //     scanf("%d", &a[i]);
+    // }
+
+    // int sum = 0;
+
+    // for (int i = 0; i < n; i++) {
+    //     sum += a[i];
+    // }
+
+    // printf("%d\n", sum);
+
+
+    //........ input array value and sum..................
+
+//     int n;
+
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i =1 ; i <= n; i++){
+//         scanf("%d", &a[i]);
+
+//         // printf("%d ",a[i]);
+//     }
+
+
+//     int sum =0;
+
+//     for (int i=1; i<=n; i++){
+//         scanf("%d",&i);
+//         sum += a[i];
+
+      
+
+
+
+//     }
+//   printf("%d",sum);
+    return 0;
+}
