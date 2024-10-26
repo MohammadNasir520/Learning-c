0a9e83157a365adcab5542af69f6723c929c2b18
MohammadNasir520 <nasirahsan520@gmail.com>
2024-10-26 23:50:28 +0600

    reverse an array by single array

--- C_Programming.c
+++ C_Programming.c
@@ -365,7 +365,7 @@ int main()
 
     // ..........delete a value from array............
 
-    /* 
+    /*
 
     int a[100000];
 
@@ -402,6 +402,33 @@ int main()
     }
 
     */
+
+    //  reverse an array by single array with temp value;
+
+    int a[100000];
+
+    int length = 0;
+
+    scanf("%d", &length);
+
+    for (int i = 0; i < length; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0, j = length - 1; i <= j; i++, j--)
+    {
+
+        int temp = a[i];
+        a[i] = a[j];
+
+        a[j] = temp;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", a[i]);
+    }
 
     return 0;
 }
